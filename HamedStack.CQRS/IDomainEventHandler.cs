565a511b6124888946312be3daed4e9819450834
hamedfathi <hamedfathi@outlook.com>
2024-04-15 07:39:31 +0200

    feat(CQRS): IDomainEventHandler added.

---
+++ HamedStack.CQRS/IDomainEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using HamedStack.TheAggregateRoot.Events;
+using MediatR;
+
+namespace HamedStack.CQRS;
+
+public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent> where TDomainEvent : DomainEvent
+{
+}
